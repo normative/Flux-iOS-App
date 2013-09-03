f50f6ec0b6d698605d895c8c62d54c6356a3a947
Kei Turner <kei@normative.com>
2013-09-03 17:37:20 -0400

    updated annotationTableCell,updated time stand to NSDate, and changed object mapping

--- SMLRcam/SMLRcam/FluxScanImageObject.m
+++ SMLRcam/SMLRcam/FluxScanImageObject.m
@@ -81,15 +81,11 @@ withDescriptionString:(NSString*)description
 }
 
 - (NSString *)generateUniqueStringID
-{
-    NSDateFormatter *inputDateFormat = [[NSDateFormatter alloc] init];
-    [inputDateFormat setDateFormat:@"yyyy'-'MM'-'dd'T'HH':'mm':'ss'Z'"];
-    NSDate *objDate = [inputDateFormat dateFromString:self.timestampString];
-    
+{    
     NSDateFormatter *outputDateFormat = [[NSDateFormatter alloc] init];
     [outputDateFormat setDateFormat:@"yyyyMMddHHmmss"];
     
-    NSString *stringID = [outputDateFormat stringFromDate:objDate];
+    NSString *stringID = [outputDateFormat stringFromDate:self.timestamp];
     return [NSString stringWithFormat:@"%@_%d", stringID, self.userID];
 }
 
