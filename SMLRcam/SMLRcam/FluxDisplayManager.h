edf76d6883042dded237fc49a73f6859c5383ddf
Kei Turner <kei@normative.com>
2013-09-24 10:23:37 -0400

    beginning the addition of the display manager

---
+++ SMLRcam/SMLRcam/FluxDisplayManager.h
@@ -0,0 +1,17 @@
+//
+//  FluxDisplayManager.h
+//  Flux
+//
+//  Created by Kei Turner on 2013-09-23.
+//  Copyright (c) 2013 Normative. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+#import "FluxDataManager.h"
+
+@interface FluxDisplayManager : NSObject
+
+@property (nonatomic, strong) FluxDataManager *fluxDataManager;
+
+@end
---
+++ SMLRcam/SMLRcam/FluxDisplayManager.m
@@ -0,0 +1,13 @@
+//
+//  FluxDisplayManager.m
+//  Flux
+//
+//  Created by Kei Turner on 2013-09-23.
+//  Copyright (c) 2013 Normative. All rights reserved.
+//
+
+#import "FluxDisplayManager.h"
+
+@implementation FluxDisplayManager
+
+@end
