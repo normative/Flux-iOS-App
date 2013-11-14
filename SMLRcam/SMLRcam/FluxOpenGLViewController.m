d0a36ff378f594e38e0042b4c6ea40e6324f8a21
Denis Delorme <denis@smlr.is>
2013-11-14 15:33:52 -0500

    Fixed small issue where not checking return of cache fetch. Could result in attempt to load NULL image.

--- SMLRcam/SMLRcam/FluxOpenGLViewController.m
+++ SMLRcam/SMLRcam/FluxOpenGLViewController.m
@@ -1353,40 +1353,48 @@ didOutputSampleBuffer:(CMSampleBufferRef)sampleBuffer
             {
                 if (tel.used == false)
                 {
-                    // found one - set it up...
-                    textureIndex = tel.textureIndex;
-                    
                     // not found so always load lowest resolution (thumb typically)
                     FluxImageType rtype = none;
                     UIImage *image = [self.fluxDisplayManager.fluxDataManager fetchImagesByLocalID:ire.localID withSize:lowest_res returnSize:&rtype];
 
-//                    NSError *error = [self loadTexture:textureIndex withImage:ire.image];
-                    NSError *error = [self loadTexture:textureIndex withImage:image];
-
-                    if (error)
+                    if (image != nil)
                     {
-                        textureIndex = -1;
+//                        NSError *error = [self loadTexture:textureIndex withImage:ire.image];
+                        NSError *error = [self loadTexture:textureIndex withImage:image];
+
+                        if (error)
+                        {
+                            textureIndex = -1;
+                        }
+                        else
+                        {
+                            // found one - set it up...
+                            
+                            if (tel.localID != nil)
+                            {
+                                // break link from old ire to tel - need to search and update it.
+                                FluxImageRenderElement *tire = [self.fluxDisplayManager getRenderElementForKey:tel.localID];
+                                if (tire != nil)
+                                {
+                                    tire.textureMapElement = nil;
+                                }
+                            }
+                            
+                            ire.textureMapElement = tel;
+                            tel.used = true;
+                            tel.localID = ire.localID;
+                            tel.imageType = rtype;
+                            justLoaded = true;
+                            textureIndex = tel.textureIndex;
+
+                            NSLog(@"Loaded Image texture in slot %d for key %@", (textureIndex),ire.localID);
+                        }
+                        break;
                     }
                     else
                     {
-                        if (tel.localID != nil)
-                        {
-                            // break link from old ire to tel - need to search and update it.
-                            FluxImageRenderElement *tire = [self.fluxDisplayManager getRenderElementForKey:tel.localID];
-                            if (tire != nil)
-                            {
-                                tire.textureMapElement = nil;
-                            }
-                        }
-                        
-                        ire.textureMapElement = tel;
-                        tel.used = true;
-                        tel.localID = ire.localID;
-                        tel.imageType = rtype;
-                        justLoaded = true;
-                        NSLog(@"Loaded Image texture in slot %d for key %@", (textureIndex),ire.localID);
+                        NSLog(@"GLVC:UpdateTextures: lowest_res texture not found in cache");
                     }
-                    break;
                 }
             }
         }
@@ -1422,6 +1430,10 @@ didOutputSampleBuffer:(CMSampleBufferRef)sampleBuffer
                         loadedOneHiRes = true;
                         NSLog(@"Updated Image texture in slot %d for key %@", (textureIndex),ire.localID);
                     }
+                }
+                else
+                {
+                    NSLog(@"GLVC:UpdateTextures: upgrade_res texture not found in cache - not updating");
                 }
             }
         }
