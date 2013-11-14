b94e7b1d46cd5320bd79d4e72cfbeff61da79121
Kei Turner <kei@normative.com>
2013-11-14 14:40:10 -0500

    added social Manager class, also added Facebook + Social frameworks

---
+++ SMLRcam/SMLRcam/FluxSocialManager.h
@@ -0,0 +1,13 @@
+//
+//  FluxSocialManager.h
+//  Flux
+//
+//  Created by Kei Turner on 11/14/2013.
+//  Copyright (c) 2013 Normative. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface FluxSocialManager : NSObject
+
+@end
