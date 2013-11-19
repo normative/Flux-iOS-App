bfd6ca1545c1097c980658869304a0e3d6f718ba
Kei Turner <kei@normative.com>
2013-11-19 16:52:48 -0500

    added edit capability to profile photos view

--- SMLRcam/SMLRcam/FluxProfilePhotosViewController.m
+++ SMLRcam/SMLRcam/FluxProfilePhotosViewController.m
@@ -7,6 +7,7 @@
 //
 
 #import "FluxProfilePhotosViewController.h"
+#import "KTCheckboxButton.h"
 
 @interface FluxProfilePhotosViewController ()
 
@@ -26,7 +27,7 @@
 - (void)viewDidLoad
 {
     [super viewDidLoad];
-    
+    removedImages = [[NSMutableArray alloc]init];
     
     picturesArray = [[NSMutableArray alloc]init];
     for (int i = 0; i<9; i++) {
@@ -36,7 +37,13 @@
     
     [self setTitle:@"My Photos"];
     
+    [garbageButton setEnabled:NO];
+    
 	// Do any additional setup after loading the view.
+}
+
+- (void)viewWillDisappear:(BOOL)animated{
+    [self.navigationController setToolbarHidden:YES animated:NO];
 }
 
 - (void)didReceiveMemoryWarning
@@ -54,7 +61,19 @@
     static NSString *identifier = @"cell";
     
     UICollectionViewCell *cell = [collectionView dequeueReusableCellWithReuseIdentifier:identifier forIndexPath:indexPath];
-    
+    if (isEditing) {
+        [[cell viewWithTag:200]setHidden:NO];
+        KTCheckboxButton*checkbox = (KTCheckboxButton*)[cell viewWithTag:200];
+        if ([removedImages containsObject:[NSNumber numberWithInt:indexPath.row]]) {
+            [checkbox setChecked:YES];
+        }
+        else{
+            [checkbox setChecked:NO];
+        }
+    }
+    else{
+        [[cell viewWithTag:200]setHidden:YES];
+    }
     UIImageView *recipeImageView = (UIImageView *)[cell viewWithTag:100];
     recipeImageView.image = [(IDMPhoto*)[picturesArray objectAtIndex:indexPath.row]underlyingImage];
     
@@ -62,14 +81,67 @@
 }
 
 - (void)collectionView:(UICollectionView *)collectionView didSelectItemAtIndexPath:(NSIndexPath *)indexPath{
-    IDMPhotoBrowser *browser = [[IDMPhotoBrowser alloc] initWithPhotos:picturesArray animatedFromView:[collectionView cellForItemAtIndexPath:indexPath].contentView];
-    [browser setInitialPageIndex:indexPath.row];
-    [browser setDelegate:self];
-    [self presentViewController:browser animated:YES completion:nil];
+    if (isEditing) {
+        if ([removedImages containsObject:[NSNumber numberWithInt:indexPath.row]]) {
+            [removedImages removeObject:[NSNumber numberWithInt:indexPath.row]];
+            if (removedImages.count == 0) {
+                [garbageButton setEnabled:NO];
+            }
+            [collectionView reloadData];
+        }
+        else{
+            [removedImages addObject:[NSNumber numberWithInt:indexPath.row]];
+            [collectionView reloadData];
+            [garbageButton setEnabled:YES];
+        }
+
+    }
+    else{
+        IDMPhotoBrowser *browser = [[IDMPhotoBrowser alloc] initWithPhotos:picturesArray animatedFromView:[collectionView cellForItemAtIndexPath:indexPath].contentView];
+        [browser setInitialPageIndex:indexPath.row];
+        [browser setDelegate:self];
+        [self presentViewController:browser animated:YES completion:nil];
+    }
 }
 
 - (void)photoBrowser:(IDMPhotoBrowser *)photoBrowser didDismissAtPageIndex:(NSUInteger)index{
     
 }
 
+- (IBAction)garbageButtonAction:(id)sender {
+    NSMutableArray *indexPaths = [[NSMutableArray alloc]initWithCapacity:removedImages.count];
+    NSMutableIndexSet *indexSet = [[NSMutableIndexSet alloc]init];
+    for (int i = 0; i<removedImages.count; i++) {
+        int index = [picturesArray indexOfObject:[picturesArray objectAtIndex:[[removedImages objectAtIndex:i]integerValue]]];
+        
+        NSIndexPath*indexPath = [NSIndexPath indexPathForRow:[[removedImages objectAtIndex:i]integerValue] inSection:0];
+        [indexPaths addObject:indexPath];
+        [indexSet addIndex:indexPath.row];
+    }
+    [picturesArray removeObjectsAtIndexes:indexSet];
+    [removedImages removeAllObjects];
+    
+    [self.collectionView performBatchUpdates:^{
+        [self.collectionView deleteItemsAtIndexPaths:indexPaths];
+        
+    } completion:^(BOOL finished) {
+        [self swapEditModes];
+    }];
+}
+
+- (IBAction)editButtonAction:(id)sender {
+    [self swapEditModes];
+}
+
+- (void)swapEditModes{
+    isEditing = !isEditing;
+    if (isEditing) {
+        [self.navigationController setToolbarHidden:NO animated:YES];
+    }
+    else{
+        [self.navigationController setToolbarHidden:YES animated:YES];
+    }
+    
+    [theCollectionView reloadData];
+}
 @end
