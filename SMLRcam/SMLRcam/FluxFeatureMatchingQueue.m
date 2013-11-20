79a6cb0ae5ee03d19c81db2b4478d18efd34c10f
Ryan Martens <ryan@smlr.is>
2013-11-20 10:54:52 -0500

    Update copyright notice to SMLR.

--- SMLRcam/SMLRcam/FluxFeatureMatchingQueue.m
+++ SMLRcam/SMLRcam/FluxFeatureMatchingQueue.m
@@ -3,7 +3,7 @@
 //  Flux
 //
 //  Created by Ryan Martens on 11/20/13.
-//  Copyright (c) 2013 Normative. All rights reserved.
+//  Copyright (c) 2013 SMLR. All rights reserved.
 //
 
 #import "FluxFeatureMatchingQueue.h"
