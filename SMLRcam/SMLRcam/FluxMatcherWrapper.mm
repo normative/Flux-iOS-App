5cc6c292100a0a2fc22191ef70494c609c1a4260
Ryan Martens <ryan@smlr.is>
2013-12-06 17:57:17 -0500

    Test to convert to center pixel frame instead of top-left in image space.

--- SMLRcam/SMLRcam/FluxMatcherWrapper.mm
+++ SMLRcam/SMLRcam/FluxMatcherWrapper.mm
@@ -171,8 +171,14 @@
         for( size_t i = 0; i < matches.size(); i++ )
         {
             //-- Get the keypoints from the good matches
-            obj.push_back( keypoints_object[ matches[i].queryIdx ].pt );
-            scene.push_back( keypoints_scene[ matches[i].trainIdx ].pt );
+            cv::Point2f tempObj = keypoints_object[ matches[i].queryIdx ].pt;
+            cv::Point2f tempScene = keypoints_scene[ matches[i].trainIdx ].pt;
+            tempObj.x = tempObj.x - object_img.cols/2;
+            tempObj.y = tempObj.y - object_img.rows/2;
+            tempScene.x = tempScene.x - scene_img.cols/2;
+            tempScene.y = tempScene.y - scene_img.rows/2;
+            obj.push_back( tempObj );
+            scene.push_back( tempScene );
         }
         
         cv::Mat H = cv::findHomography( obj, scene, CV_LMEDS );
