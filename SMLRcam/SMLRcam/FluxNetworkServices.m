3c5087830090d9a0b096031dc6a7042724c69c54
Denis Delorme <denis@smlr.is>
2014-01-03 14:02:47 -0500

    Add commented out RestKit logging commands.  No net functional change. Re-jig server URL definitions such that only the NSString definition needs to be changed when switching between servers for test. No net functional change.

--- SMLRcam/SMLRcam/FluxNetworkServices.m
+++ SMLRcam/SMLRcam/FluxNetworkServices.m
@@ -16,13 +16,13 @@
 
 
 NSString* const FluxProductionServerURL = @"http://54.221.254.230/";
+//NSString* const FluxProductionServerURL = @"http://54.221.222.71/";
+//NSString* const FluxProductionServerURL = @"http://192.168.2.18:3101/";
 NSString* const FluxTestServerURL = @"http://54.221.222.71/";
 
 //serverURL
-#define productionServerURL @"http://54.221.254.230/"
-//#define productionServerURL @"http://54.221.222.71/"
-#define testServerURL @"http://54.221.222.71/"
-//#define productionServerURL @"http://192.168.2.18:3001/"
+#define productionServerURL FluxProductionServerURL
+#define testServerURL   FluxTestServerURL
 
 @implementation FluxNetworkServices
 
