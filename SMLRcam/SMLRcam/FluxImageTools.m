5015857fe86479a26e1c91f026064c4820e11e25
Ryan Martens <ryan@smlr.is>
2014-01-29 11:36:48 -0500

    Cleanup of blurImage routine for readability. There is a leak here, but small and not going to track it down now. Root cause: image created by createCGImage:fromRect needs to be freed when we are done with it.

--- SMLRcam/SMLRcam/FluxImageTools.m
+++ SMLRcam/SMLRcam/FluxImageTools.m
@@ -13,22 +13,17 @@
 -(UIImage*)blurImage:(UIImage *)img withBlurLevel:(float)blurLevel{
     //CGImage blows away image metadata, keep orientation
     UIImageOrientation orientation = img.imageOrientation;
+
+    CIContext *context = [CIContext contextWithOptions:nil];
     
     CIImage *inputImage = [[CIImage alloc] initWithCGImage:img.CGImage];
-    CIContext *context = [CIContext contextWithOptions:nil];
     CGAffineTransform transform = CGAffineTransformIdentity;
-    CIImage *outputImage;
-    
-//    CIFilter *darkenFilter= [CIFilter filterWithName:@"CIColorControls"];
-//    [darkenFilter setValue:inputImage forKey:@"inputImage"];
-//    [darkenFilter setValue:[NSNumber numberWithFloat:0.5] forKey:@"inputBrightness"];
-//    outputImage = [darkenFilter outputImage];
     
     //clamp the borders so the blur doesnt shrink the borders of the image
     CIFilter *clampFilter = [CIFilter filterWithName:@"CIAffineClamp"];
     [clampFilter setValue:inputImage forKey:kCIInputImageKey];
     [clampFilter setValue:[NSValue valueWithBytes:&transform objCType:@encode(CGAffineTransform)] forKey:@"inputTransform"];
-    outputImage = [clampFilter outputImage];
+    CIImage *outputImage = [clampFilter outputImage];
     
     float blur = blurLevel*50;
     
@@ -36,9 +31,6 @@
     CIFilter *blurFilter = [CIFilter filterWithName:@"CIGaussianBlur" keysAndValues:kCIInputImageKey, outputImage, @"inputRadius", [NSNumber numberWithFloat:blur], nil];
     outputImage = [blurFilter outputImage];
 
-    
-
-    
     //output the image
     CGImageRef cgimg = [context createCGImage:outputImage fromRect:inputImage.extent];
     UIImage *blurredImage = [UIImage imageWithCGImage:cgimg scale:1.0 orientation:orientation];
