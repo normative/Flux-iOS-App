53a8e29cdebdc3c11c9936bd1c9a696cb0717c9a
Ryan Martens <ryan@smlr.is>
2014-02-21 14:26:59 -0500

    Hooks in place to make number of textures a parameter (was mostly in place - just put a placeholder and did some testing). Note that more than 5 textures doesn't actually work anyways (since the shader code is not correct beyond 5). Also, this code needs to make a call to determine what value should be used for this parameter at startup.

--- SMLRcam/SMLRcam/FluxOpenGLViewController.m
+++ SMLRcam/SMLRcam/FluxOpenGLViewController.m
@@ -29,7 +29,7 @@ err = glGetError();								\
 
 const float MAX_IMAGE_RADIUS = 15.0;
 
-const int number_textures = 5;
+const int default_number_textures = 5;
 
 // Uniform index.
 enum
@@ -1148,6 +1148,16 @@ didOutputSampleBuffer:(CMSampleBufferRef)sampleBuffer
     [super viewDidLoad];
     
     _displayListHasChanged = 0;
+    
+    // TODO: Make a call here to set the maximum number of textures
+    number_textures = 3;
+    
+    if (number_textures > (MAX_TEXTURES - 1))
+    {
+        // Need to keep a texture for the background
+        NSLog(@"Requested number of textures exceeds maximum. Setting to default value of %d.", default_number_textures);
+        number_textures = default_number_textures;
+    }
 
     self.renderList = [[NSMutableArray alloc] initWithCapacity:number_textures];
     
