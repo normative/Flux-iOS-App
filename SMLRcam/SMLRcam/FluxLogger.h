af9146ad49850ea0c460835a07dfe68cdbd05302
Ryan Martens <ryan@smlr.is>
2014-02-26 17:36:24 -0500

    Add file/terminal logging using CocoaLumberjack. DEBUG builds are verbose, RELEASE builds are error.

---
+++ SMLRcam/SMLRcam/FluxLogger.h
@@ -0,0 +1,18 @@
+//
+//  FluxLogger.h
+//  Flux
+//
+//  Created by Ryan Martens on 2/26/14.
+//  Copyright (c) 2014 Normative. All rights reserved.
+//
+
+#ifndef Flux_FluxLogger_h
+#define Flux_FluxLogger_h
+
+#import "DDLog.h"
+#import "DDFileLogger.h"
+#import "DDTTYLogger.h"
+
+extern int const ddLogLevel;
+
+#endif
