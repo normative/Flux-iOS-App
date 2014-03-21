4cb33334747b10069c3a9948c2d0d90175c1b495
Ryan Martens <ryan@smlr.is>
2014-03-21 10:32:26 -0400

    Clean up newly added math helper functions.

--- SMLRcam/SMLRcam/FluxMotionManagerSingleton.m
+++ SMLRcam/SMLRcam/FluxMotionManagerSingleton.m
@@ -106,29 +106,6 @@ const float pitch_polar_region_limit_up = (180.0 - 5.0) * M_PI / 180.0;
     }
 }
 
-- (GLKVector2)calcMagnetometerInEarthFrameWithPose:(GLKQuaternion *)quat andMagnetometer:(GLKVector3 *)m_vector
-{
-    GLKVector3 m_earth = GLKVector3Normalize(GLKQuaternionRotateVector3(*quat, *m_vector));
-    GLKVector3 m_projected_earth = [self projectVector:m_earth ontoPlaneWithNormal:GLKVector3Make(0.0, 0.0, 1.0)];
-    return GLKVector2Make(m_projected_earth.x, m_projected_earth.y);
-}
-
-- (double)angleDiffWithAngleA:(double)a andAngleB:(double)b
-{
-    double dif = fmod(b - a + M_PI, 2.0 * M_PI);
-    if (dif < 0.0)
-        dif += 2.0 * M_PI;
-    return dif - M_PI;
-}
-
-- (double)constrainAngle:(double)x
-{
-    x = fmod(x + M_PI, 2.0 * M_PI);
-    if (x < 0.0)
-        x += 2.0 * M_PI;
-    return x - M_PI;
-}
-
 - (void)calcAttitudeFromDeviceMotion:(CMDeviceMotion *)devMotion andHeading:(CLHeading *)heading intoQuaternion:(CMQuaternion *)outquat
 {
     // Phone reference frame: x right, y top, z up from screen
@@ -148,7 +125,7 @@ const float pitch_polar_region_limit_up = (180.0 - 5.0) * M_PI / 180.0;
     {
 //        GLKVector3 m_vector = GLKVector3Make(devMotion.magneticField.field.x, devMotion.magneticField.field.y, devMotion.magneticField.field.z);
         GLKVector3 m_vector = GLKVector3Make(heading.x, heading.y, heading.z);
-        m_t0 = [self calcMagnetometerInEarthFrameWithPose:&quat_orig andMagnetometer:&m_vector];
+        m_t0 = [self calcVectorProjectionInEarthFrameWithPose:&quat_orig andVector:&m_vector];
         
         if (!isnan(m_t0.x) && !isnan(m_t0.y))
         {
@@ -159,7 +136,7 @@ const float pitch_polar_region_limit_up = (180.0 - 5.0) * M_PI / 180.0;
     {
 //        GLKVector3 m_vector = GLKVector3Make(devMotion.magneticField.field.x, devMotion.magneticField.field.y, devMotion.magneticField.field.z);
         GLKVector3 m_vector = GLKVector3Make(heading.x, heading.y, heading.z);
-        GLKVector2 m_earth = [self calcMagnetometerInEarthFrameWithPose:&quat_orig andMagnetometer:&m_vector];
+        GLKVector2 m_earth = [self calcVectorProjectionInEarthFrameWithPose:&quat_orig andVector:&m_vector];
         
         delta_yaw_temp = [self calculateSignedAngleBetween2DVector:m_earth andVector:m_t0];
         
@@ -189,6 +166,15 @@ const float pitch_polar_region_limit_up = (180.0 - 5.0) * M_PI / 180.0;
     outquat->w = quat_final.w;
 }
 
+// Calculate 2D earth-plane projection of input vector (in device frame) in the base reference frame specified by device pose
+- (GLKVector2)calcVectorProjectionInEarthFrameWithPose:(GLKQuaternion *)quat andVector:(GLKVector3 *)v_vector
+{
+    GLKVector3 z_axis_earth = GLKVector3Make(0.0, 0.0, 1.0);
+    GLKVector3 v_earth = GLKVector3Normalize(GLKQuaternionRotateVector3(*quat, *v_vector));
+    GLKVector3 v_projected_earth = [self projectVector:v_earth ontoPlaneWithNormal:z_axis_earth];
+    return GLKVector2Make(v_projected_earth.x, v_projected_earth.y);
+}
+
 - (GLKVector3) projectVector:(GLKVector3)v ontoPlaneWithNormal:(GLKVector3)n
 {
     // A projection of a vector v onto a plane P (with normal n) is u:
@@ -215,6 +201,24 @@ const float pitch_polar_region_limit_up = (180.0 - 5.0) * M_PI / 180.0;
     // Uses the 2D Perp Dot Product to find the signed angle
     // http://mathworld.wolfram.com/PerpDotProduct.html
     return atan2(a.x*b.y - a.y*b.x, GLKVector2DotProduct(a, b));
+}
+
+// Calculates the difference (B-A) between two angles, taking into account wrap-around
+- (double)angleDiffWithAngleA:(double)a andAngleB:(double)b
+{
+    double dif = fmod(b - a + M_PI, 2.0 * M_PI);
+    if (dif < 0.0)
+        dif += 2.0 * M_PI;
+    return dif - M_PI;
+}
+
+// Constrains an input angle (in radians) to +/-180 degrees
+- (double)constrainAngle:(double)x
+{
+    x = fmod(x + M_PI, 2.0 * M_PI);
+    if (x < 0.0)
+        x += 2.0 * M_PI;
+    return x - M_PI;
 }
 
 - (euler_angles) calculateAngleSequence313FromQuaterion:(GLKQuaternion *)q
