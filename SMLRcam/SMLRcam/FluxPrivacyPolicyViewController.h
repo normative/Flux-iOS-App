13c3686fab499079577e351b0531438105fad2ff
Kei Turner <kei@normative.com>
2014-04-09 16:46:11 -0400

    added (and left unconnected) FluxPrivacyPolicyVC

---
+++ SMLRcam/SMLRcam/FluxPrivacyPolicyViewController.h
@@ -0,0 +1,15 @@
+//
+//  FluxPrivacyPolicyViewController.h
+//  Flux
+//
+//  Created by Kei Turner on 2014-04-09.
+//  Copyright (c) 2014 Normative. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "GAITrackedViewController.h"
+
+@interface FluxPrivacyPolicyViewController : GAITrackedViewController
+@property (strong, nonatomic) IBOutlet UIWebView *webView;
+
+@end
---
+++ SMLRcam/SMLRcam/FluxPrivacyPolicyViewController.m
@@ -0,0 +1,24 @@
+//
+//  FluxPrivacyPolicyViewController.m
+//  Flux
+//
+//  Created by Kei Turner on 2014-04-09.
+//  Copyright (c) 2014 Normative. All rights reserved.
+//
+
+#import "FluxPrivacyPolicyViewController.h"
+
+@implementation FluxPrivacyPolicyViewController
+
+
+- (void)viewDidLoad{
+    [super viewDidLoad];
+    self.screenName = @"Privacy Policy";
+    
+    NSString *fullURL = @"http://www.smlr.is/privacy-policy";
+    NSURL *url = [NSURL URLWithString:fullURL];
+    NSURLRequest *requestObj = [NSURLRequest requestWithURL:url];
+    [self.webView loadRequest:requestObj];
+}
+
+@end
