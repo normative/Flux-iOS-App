e4895dc15d462cdaf991cd7099a63c2bdaa7033b
Kei Turner <kei@normative.com>
2014-04-14 17:32:09 -0400

    re-disabled image capture for Kalman

--- SMLRcam/SMLRcam/FluxScanViewController.m
+++ SMLRcam/SMLRcam/FluxScanViewController.m
@@ -984,7 +984,7 @@ NSString* const FluxScanViewDidAcquireNewPictureLocalIDKey = @"FluxScanViewDidAc
     
     if (![self.fluxDisplayManager.locationManager isKalmanSolutionValid])
     {
-//        [self.cameraButton setAlpha:0.4];
+        [self.cameraButton setAlpha:0.4];
     }
     
     debugPressCount = 0;
