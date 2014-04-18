50312681c56765356e4d0906ba7555ddeb347fc5
Denis Delorme <denis@smlr.is>
2014-04-18 08:36:50 -0400

    Add secure connection note and update to secure server for production use.

--- SMLRcam/SMLRcam/FluxNetworkServices.m
+++ SMLRcam/SMLRcam/FluxNetworkServices.m
@@ -24,7 +24,7 @@
 #define _AWSTestServerURL        @"http://54.221.222.71/"
 #define _DSDLocalTestServerURL   @"http://192.168.2.12:3101/"
 
-#define _AWSSecureProductionServerURL  @"http://production.flux.smlr.is/"
+#define _AWSSecureProductionServerURL  @"https://production.flux.smlr.is/"
 #define _AWSSecureStagingServerURL     @"https://staging.flux.smlr.is/"
 
 NSString* const AWSProductionServerURL = _AWSProductionServerURL;
